ae6fa0ed14b0ef1f50dcc2d2c3dcacd4dbea8fb3
Valery Klapatsiuk <valery.klapatsiuk@gmail.com>
2021-10-01 00:20:59 +0300

    minor codestyle edits

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -2,7 +2,7 @@ function isEmpty(obj) {
   let result = 0;
 
   for (let key in obj) {
-    if (key in obj) {result += 1;}
+    if (key in obj) result += 1;
   }
 
   return (result > 0) ? false : true;
