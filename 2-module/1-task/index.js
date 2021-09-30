ebdec513a7d1b1dfce4af61de20a5104cd308466
uxpat <1073756+uxpat@users.noreply.github.com>
2021-10-01 02:48:42 +0300

    Add files via upload

---
+++ 2-module/1-task/index.js
@@ -0,0 +1,26 @@
+function sumSalary(salaries) {
+  let sum = 0; 
+  for (let key in salaries) {
+    if (salaries[key] === Number(salaries[key]) &&
+        ![NaN, Infinity, -Infinity].includes(salaries[key])) {
+      sum += salaries[key];
+    }
+  }
+  return sum;
+}
+
+// Konstantin's solution:
+
+// function sumSalary(salaries) {
+//   let sumOfSalaries = 0;
+
+//   for (let key in salaries) {
+//     let isSalary = typeof salaries[key] === 'number';
+
+//     if (isSalary) {
+//       sumOfSalaries += salaries[key];
+//     }
+//   }
+
+//   return sumOfSalaries;
+// }
