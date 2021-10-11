59d4bd25fea614de4e4115d83fdd1766f485f5c4
Valery Klapatsiuk <valery.klapatsiuk@gmail.com>
2021-10-11 23:25:06 +0300

    Initial commit: 'highlight' function

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+  for (let tr of table.rows) {
+    for (let td of tr.cells) {
+      switch (table.tHead.rows[0].cells[td.cellIndex].innerHTML) {
+      case 'Status':
+        if (!td.dataset.available) {
+          tr.hidden = true;
+        } else if (td.dataset.available === 'true') {
+          tr.classList.toggle('available');
+        } else if (td.dataset.available === 'false') {
+          tr.classList.toggle('unavailable');
+        }
+      case 'Gender':
+        if (td.innerHTML === 'm') {
+          tr.classList.add('male');
+        } else if (td.innerHTML === 'f') {
+          tr.classList.add('female');
+        }
+      case 'Age':
+        if (td.innerHTML < 18) {
+          tr.style.textDecoration = 'line-through';
+        }
+      }
+    }
+  }
 }
